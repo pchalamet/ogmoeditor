c230b78b60ba9cd1e3a7eb03d315bcefd10081c4
Matt <Matt@Lappy6000.home>
2011-10-02 10:55:01 -0600

    Missing File

---
+++ OgmoEditor/Project.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace OgmoEditor
+{
+    [Serializable()]
+    public class Project
+    {
+        public string Name;
+        public string WorkingDirectory;
+
+        [NonSerialized()]
+        public bool Changed = false;
+
+        public Project()
+        {
+            Name = "New Project";
+            WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        public void Save(string filename)
+        {
+            Changed = false;
+
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(new FileStream(filename, FileMode.OpenOrCreate), this);
+        }
+    }
+}
