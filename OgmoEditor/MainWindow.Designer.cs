1dc6729f068ad4b6891c783aec3061802088d060
Matthew <Matthew@Matthew-PC.home>
2011-10-10 14:09:53 -0700

    Backed out changeset: f0804923d42b

--- OgmoEditor/MainWindow.Designer.cs
+++ OgmoEditor/MainWindow.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.StatusStrip = new System.Windows.Forms.StatusStrip();
             this.MainStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.mouseCoordinatesLabel = new System.Windows.Forms.ToolStripStatusLabel();
@@ -65,6 +64,9 @@
             // 
             // StatusStrip
             // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MainStatusLabel,
+            this.mouseCoordinatesLabel});
             this.StatusStrip.Location = new System.Drawing.Point(0, 540);
             this.StatusStrip.Name = "StatusStrip";
             this.StatusStrip.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -86,6 +88,11 @@
             // 
             // MenuStrip
             // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.projectToolStripMenuItem,
+            this.levelToolStripMenuItem,
+            this.viewToolStripMenuItem});
             this.MenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MenuStrip.Name = "MenuStrip";
             this.MenuStrip.Size = new System.Drawing.Size(784, 24);
@@ -315,7 +322,6 @@
             this.Controls.Add(this.masterTabControl);
             this.Controls.Add(this.StatusStrip);
             this.Controls.Add(this.MenuStrip);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.MenuStrip;
             this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "MainWindow";
