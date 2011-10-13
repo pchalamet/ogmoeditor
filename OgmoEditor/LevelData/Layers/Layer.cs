8ea6821089e10f5acfd2eee8d5139affa27a7ff8
Matthew <Matthew@Matthew-PC.home>
2011-10-12 19:03:47 -0700

    Got rid of levelCanvas

---
+++ OgmoEditor/LevelData/Layers/Layer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OgmoEditor.Definitions.LayerDefinitions;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OgmoEditor.LevelData.Layers
+{
+    public class Layer
+    {
+        public LayerDefinition Definition { get; private set; }
+
+        public Layer(LayerDefinition definition)
+        {
+            Definition = definition;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
--- OgmoEditor/LevelEditors/LevelEditor.cs
+++ OgmoEditor/LevelEditors/LevelEditor.cs
@@ -21,7 +21,6 @@ namespace OgmoEditor.LevelEditors
         private Level level;
         private Content content;
         private SpriteBatch spriteBatch;
-        private RenderTarget2D levelCanvas;
         private bool mouseMoveMode;
         private Point lastMousePoint;
 
@@ -44,7 +43,6 @@ namespace OgmoEditor.LevelEditors
             Camera.Origin = new Vector2(Width / 2, Height / 2);
             centerCamera();
             DrawBounds = new Rectangle(0, 0, Width, Height);
-            initLevelCanvas();
 
             //Load the content
             content = new Content(GraphicsDevice);
@@ -62,13 +60,6 @@ namespace OgmoEditor.LevelEditors
             this.MouseWheel += onMouseWheel;
         }
 
-        private void initLevelCanvas()
-        {
-            if (levelCanvas != null)
-                levelCanvas.Dispose();
-            levelCanvas = new RenderTarget2D(GraphicsDevice, level.Size.Width, level.Size.Height); 
-        }
-
         private void centerCamera()
         {
             Camera.X = level.Size.Width / 2;
@@ -77,13 +68,6 @@ namespace OgmoEditor.LevelEditors
 
         protected override void Draw()
         {
-            //Draw the level onto the level canvas
-            GraphicsDevice.SetRenderTarget(levelCanvas);
-            GraphicsDevice.Clear(Ogmo.Project.BackgroundColor.toXNA());
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, RasterizerState.CullNone);
-            
-            spriteBatch.End();
-
             //Draw the background and logo
             GraphicsDevice.SetRenderTarget(null);
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, RasterizerState.CullNone);
@@ -94,7 +78,7 @@ namespace OgmoEditor.LevelEditors
             //Draw the level onto the control, positioned and scaled by the camera
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, RasterizerState.CullNone, null, Camera.Matrix);
             content.DrawRectangle(spriteBatch, 10, 10, level.Size.Width, level.Size.Height, new Color(0, 0, 0, .5f));
-            spriteBatch.Draw(levelCanvas, Vector2.Zero, Color.White);
+            content.DrawRectangle(spriteBatch, 0, 0, level.Size.Width, level.Size.Height, Ogmo.Project.BackgroundColor.toXNA());
             spriteBatch.End();
         }
 
