7a886885ad5c934a6d78f243ff5ce91a53dd0589
Matthew <Matthew@Matthew-PC.home>
2011-10-16 12:44:05 -0700

    Entity remove action

---
+++ OgmoEditor/LevelEditors/Actions/EntityActions/EntityRemoveAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OgmoEditor.LevelData.Layers;
+
+namespace OgmoEditor.LevelEditors.Actions.EntityActions
+{
+    public class EntityRemoveAction : EntityAction
+    {
+        private List<Entity> toRemove;
+
+        public EntityRemoveAction(EntityLayer entityLayer, List<Entity> entities)
+            : base(entityLayer)
+        {
+            toRemove = entities;
+        }
+
+        public override void Do()
+        {
+            foreach (var e in toRemove)
+                EntityLayer.Entities.Remove(e);
+        }
+
+        public override void Undo()
+        {
+            foreach (var e in toRemove)
+                EntityLayer.Entities.Add(e);
+        }
+    }
+}
