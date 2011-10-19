576e06c58cd9e107c83f2c30d5e7d23c12ac9a06
Matthew <Matthew@Matthew-PC.home>
2011-10-18 20:48:49 -0700

    Value w/ get/set XML

---
+++ OgmoEditor/LevelData/Layers/Value.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OgmoEditor.Definitions.ValueDefinitions;
+using System.Xml;
+
+namespace OgmoEditor.LevelData.Layers
+{
+    public class Value
+    {
+        public ValueDefinition Definition { get; private set; }
+        public string val;
+
+        public Value(ValueDefinition definition)
+        {
+            Definition = definition;
+            val = definition.GetDefault();
+        }
+
+        public XmlAttribute GetXML(XmlDocument doc)
+        {
+            XmlAttribute xml = doc.CreateAttribute(Definition.Name);
+            xml.InnerText = val;
+            return xml;
+        }
+
+        public void SetXML(XmlAttribute xml)
+        {
+            val = xml.InnerText;
+        }
+    }
+}
