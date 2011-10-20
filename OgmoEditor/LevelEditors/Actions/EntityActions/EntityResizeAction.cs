ff7ddcc6d09290815ea52c0962ecca43a992d822
Matthew <Matthew@Matthew-PC.home>
2011-10-19 18:21:23 -0700

    Start of resize action

---
+++ OgmoEditor/LevelEditors/Actions/EntityActions/EntityResizeAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OgmoEditor.LevelData.Layers;
+using System.Drawing;
+
+namespace OgmoEditor.LevelEditors.Actions.EntityActions
+{
+    public class EntityResizeAction : EntityAction
+    {
+        private List<Entity> entities;
+        private Point resize;
+
+        public EntityResizeAction(EntityLayer entityLayer, List<Entity> entities, Point resize)
+            : base(entityLayer)
+        {
+            this.entities = entities;
+            this.resize = resize;
+        }
+    }
+}
