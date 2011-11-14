ce4000f3945ade8dd8019984f18769674972a1e7
Matthew <Matthew@Matthew-PC.home>
2011-11-14 12:24:31 -0800

    Insert/RemoveNode actions

---
+++ OgmoEditor/LevelEditors/Actions/EntityActions/EntityInsertNodeAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OgmoEditor.LevelData.Layers;
+using System.Drawing;
+
+namespace OgmoEditor.LevelEditors.Actions.EntityActions
+{
+    public class EntityInsertNodeAction : EntityAction
+    {
+        private Entity entity;
+        private Point node;
+        private int index;
+
+        public EntityInsertNodeAction(EntityLayer entityLayer, Entity entity, Point node, int index)
+            : base(entityLayer)
+        {
+            this.entity = entity;
+            this.node = node;
+            this.index = index;
+        }
+
+        public override void Do()
+        {
+            entity.Nodes.Insert(index, node);
+        }
+
+        public override void Undo()
+        {
+            entity.Nodes.RemoveAt(index);
+        }
+    }
+}
--- OgmoEditor/LevelEditors/Tools/EntityTools/EntityInsertNodeTool.cs
+++ OgmoEditor/LevelEditors/Tools/EntityTools/EntityInsertNodeTool.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OgmoEditor.LevelData.Layers;
+using System.Drawing;
 
 namespace OgmoEditor.LevelEditors.Tools.EntityTools
 {
@@ -10,7 +12,12 @@ namespace OgmoEditor.LevelEditors.Tools.EntityTools
         public EntityInsertNodeTool()
             : base("Insert Node", "insertNode.png", System.Windows.Forms.Keys.I)
         {
-
+            public EntityInsertNodeAction(EntityLayer entityLayer, Entity entity, Point node)
+            : base(entityLayer)
+        {
+            this.entity = entity;
+            this.node = node;
+        }
         }
     }
 }
