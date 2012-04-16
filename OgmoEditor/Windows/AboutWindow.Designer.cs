6c24f15e816178fd611cd86ec02fa24b481e7475
Matthew <Matthew@Matthew-PC.home>
2012-04-16 14:22:36 -0700

    Made the About window non-resizable

--- OgmoEditor/Windows/AboutWindow.Designer.cs
+++ OgmoEditor/Windows/AboutWindow.Designer.cs
@@ -142,6 +142,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
